15ac088719460931510bdbfd96935f818e755467
makoyamii <sanjana.ravikumar77@gmail.com>
2024-07-06 21:01:12 -0500

    batch 2

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ const textContainer = document.querySelector("#text");
 
 
 const len = document.querySelector('#len');
-const glowstick = document.querySelector('#glowstick');
+const mikustick = document.querySelector('#mikustick');
+const rinstick = document.querySelector('#rinstick');
+const lukastick = document.querySelector('#lukastick');
+const meikostick = document.querySelector('#meikostick');
+const kaitostick = document.querySelector('#kaitostick');
 
 //seekbar info
 const seekbar = document.querySelector("#seekbar");
@@ -45,6 +49,7 @@ let count = 0;
 let points = 0;
 let stop = false;
 let pause = false;
+
 
 
 
@@ -77,7 +82,11 @@ function onAppMediaChange() {
       // Reset screen display
       loading.className = "";
       len.className = "";
-      glowstick.className = "";
+      mikustick.className = "";
+      rinstick.className = "";
+      lukastick.className = "";
+      meikostick.className = "";
+      kaitostick.className = "";
       resetChars();
 }
   
@@ -94,38 +103,20 @@ function onVideoReady(video){
 function onTimerReady() {
     loading.className = "disabled";
     len.className = "";
-    glowstick.className = ""; 
+    mikustick.className = ""; 
+    rinstick.className = "";
+    lukastick.className = "";
+    meikostick.className = "";
+    kaitostick.className = "";
     document.querySelector("#player > a#play").className = "";
     document.querySelector("#player > a#stop").className = "";
-                /* show phrase byt phrase
-                //Interface IVideo may be helpful
-                let w = player.video.firstWord;
-                let p = player.video.firstPhrase;
-  
-                while(p && p.next) {
-                  p.animate = animatePhrase;
-                  p = p.next; 
-                }
-                */
+
     
     
 }
 
 
 
-
-
-//LYRICS
-/*
-function animatePhrase (now, unit) {
-    if (unit.contains(now)) {
-        textContainer.textContent = unit.text;
-    }
-}
-*/
-  
-  
-  
   
 //UPDATING THE SEEKBAR/PROGRESS BAR AND LYRICS
 function onTimeUpdate(position) {
@@ -140,10 +131,18 @@ function onTimeUpdate(position) {
         //active makes len jump on beat
         requestAnimationFrame(() => {
           len.className = "active";
-          glowstick.className = "active";
+          mikustick.className = "active";
+          rinstick.className = "active";
+          lukastick.className = "active";
+          meikostick.className = "active";
+          kaitostick.className = "active";
           requestAnimationFrame(() => {
             len.className = "beat";
-            glowstick.className = "beat";
+            mikustick.className = "beat";
+            rinstick.className = "beat";
+            lukastick.className = "beat";
+            meikostick.className = "beat";
+            kaitostick.className = "beat";
           });
         });
 
@@ -189,29 +188,39 @@ function onTimeUpdate(position) {
 //PLAY
 function onPlay() {
     const a = document.querySelector("#player > a#play");
+    //console.log("play");
     while (a.firstChild) a.removeChild(a.firstChild); 
-    a.appendChild(document.createTextNode("\uf28b"));
+    a.appendChild(document.createTextNode("►"));
   pause = false;
 }
     
 //PAUSE
 function onPause() {
+  
     const a = document.querySelector("#player > a#play");
     while (a.firstChild) a.removeChild(a.firstChild);
-    a.appendChild(document.createTextNode("\uf144"));
+    a.appendChild(document.createTextNode("►"));
   pause = true;
+  
 }
   
 
 
 //PLAY/PAUSE
+
 document.querySelector("#player > a#play").addEventListener("click", (e) => {
   e.preventDefault();
   if(player) {
     if(player.isPlaying) {
+      /*
       player.requestPause();
       len.className = "";
-      glowstick.className = "";
+      mikustick.className = "";
+      rinstick.className = "";
+      lukastick.className = "";
+      meikostick.className = "";
+      kaitostick.className = "";
+      */
     } else {
       player.requestPlay();
     }
@@ -219,68 +228,29 @@ document.querySelector("#player > a#play").addEventListener("click", (e) => {
   return false;
 });
 
+
 //STOP
 document.querySelector("#player > a#stop").addEventListener("click", (e) => {
   e.preventDefault();
   if (player) {
     player.requestStop();
     len.className = "";
-    glowstick.className = "";
+    mikustick.className = "";
+    rinstick.className = "";
+    lukastick.className = "";
+    meikostick.className = "";
+    kaitostick.className = "";
     points = 0;
     count = 0;
     pointbar.style.height = parseInt(0) + 'px';
-    progressbar.style.height = parseInt(0) + 'px';
+    //progressbar.style.height = parseInt(0) + 'px';
     stop = true;
   }
   return false;
 });
 
-//SEEKBAR CLICK TO LOCATION
-/*
-seekbar.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (player) {
-    player.requestMediaSeek(
-      (player.video.duration * e.offsetX) / seekbar.clientWidth
-    );
-    
-    len.className = "";
-  }
-  return false;
-});
-*/
-
-
-//ADD BANANAS
-/*
-document.body.addEventListener('click', function(e) {
-
-    const img = document.createElement('img');
-  
-    img.style.height = '50px';
-    img.style.width = '50px';
-        var x = event.pageX - this.offsetLeft;
-        var y = event.pageY - this.offsetTop;
-        console.log("X Coordinate: " + x + " Y Coordinate: " + y);
-  img.style.position = 'absolute';
-  img.style.left = x + "px";
-  img.style.top = y + "px";
-  img.style.zIndex = 4;
-  img.setAttribute('src','https://static.vecteezy.com/system/resources/thumbnails/013/743/633/small_2x/banana-pixel-art-png.png');
-    
-    document.body.appendChild(img);
-});
-*/
-
-
-
-
-//display text 
-  //when you click the text, the character disappears
-  //clicking on the text seems to interrupt the phrase
 
 //when you click on text, points go up
-
 //called when a new character is vocalized
 function newChar(current) {
   let phrase = player.video.phrases[count];
@@ -290,31 +260,29 @@ function newChar(current) {
   container.appendChild(document.createTextNode(current.text));
   
   
-  //click will get the current character's start time and rewind
-  /*
-  container.addEventListener("click", () => {
-    player.requestMediaSeek(current.startTime);
-  });
-  */
-  
-  //TO REMOVE SPECIFIC CHARACTERS, EVERY CHARACTER NEEDS TO BE WITHIN A SPAN?
-  
   
   //if it gets too long, reset
   if(count == 0) {
-    if(textContainer.textContent.length > 21 && !stop) {
+    if(textContainer.textContent.length > 37 && !stop) {
+      console.log(count);
+      console.log(textContainer.textContent);
+      console.log((phrase).toString());
       resetChars();
       count++;
-      //console.log("FIRST TRY");
+      console.log("FIRST TRY");
     } else if (textContainer.textContent.length > 17 && stop) {
+      console.log(count);
+      console.log(textContainer.textContent);
+      console.log((phrase).toString());
       resetChars();
       count++;
-      //console.log("FIRST TRY WITH STOP");      
+      console.log("FIRST TRY WITH STOP");      
     }
   } else if(textContainer.textContent.length == phrase.toString().length) {
+    console.log(count);
+    console.log(textContainer.textContent);
+    console.log((phrase).toString());
     
-    //console.log((phrase).toString());
-    //console.log(count);
     resetChars();
     count++;
     //Icanbea“SUPERHERO” - 18 char
@@ -334,7 +302,7 @@ function newChar(current) {
       if(pointbar.offsetHeight < 500) {
         pointbar.style.height = parseInt(points)/3 + 'px';
       }
-      console.log(points); 
+      //console.log(points); 
       //pointbar.style.height = `${parseInt(points)}`;
     }
     //console.log(e.target);
